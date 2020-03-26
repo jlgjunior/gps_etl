c05de7c2b8839fdf15fa7a15320a02b540d20ee2
jlgjunior <jgaribaldijunior@gmail.com>
2020-03-26 14:12:47 -0300

    Process data points files

--- DataReader.py
+++ DataReader.py
@@ -1,3 +1,21 @@
+import os
+from os import listdir
 
+class DataReader():
 
-class DataReader:
+  def __init__(self, coordinateFilesPath):
+    self._coordinateFilesPath = coordinateFilesPath
+
+  def readCoordinates(self):
+    coordinates = []
+    for fileName in listdir(self._coordinateFilesPath):
+      file = open(os.path.join(self._coordinateFilesPath, fileName))
+      for line in file: 
+        value = line.strip().split(' ')[4]
+        if line.startswith('La'):
+          coordinate = [float(value)]
+        elif line.startswith('Lo'):
+          coordinate.append(float(value))
+          coordinates.append(coordinate)
+    return coordinates
+      
